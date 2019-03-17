974eea477993becc0f44401c375508795673370e
Dan Hwang <danhwangfkpc@gmail.com>
2019-03-17 20:58:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,8 @@ def oxford_comma(array)
   elsif array.length == 1
   	array.join
   elsif array.length > 2
-    array[-1] << "and "
-    array.join(", ")
+    # array[-1] << "and "
+    # array.join(", ")
+    new_array = #{array[0..1].join(', ')} and #{array.last}
   end
 end
