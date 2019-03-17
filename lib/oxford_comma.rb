7ece25e1ce98b6e7ac176d9dc2364d1c04acbf0c
Dan Hwang <danhwangfkpc@gmail.com>
2019-03-17 21:01:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 2
+  array[-2] << " and "
+  array.join
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+  
+    new_array = "#{array[0...-1].join(", ")}, and #{array.last}"
+    new_array
+  end
 end
